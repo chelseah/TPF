fixed minor bugs in phasefit.cc and changed the output precision.

plottranmap.py
@@ -4,6 +4,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 from matplotlib import cm
 def main():
+    #infile = 'Kepler36b'
     infile = 'temp'
     data = np.loadtxt(infile)
     mdat = np.ma.masked_array(data,data==0)
@@ -13,7 +14,7 @@ def main():
     plt.imshow(mdat)
     plt.xlabel("Nbin")
     plt.ylabel("Ntran")
-    plt.title("Transit and NonTTV-model")
+    plt.title("Single Outlier model")
     plt.colorbar()
     plt.show()
     return

test.py
@@ -8,33 +8,37 @@ import fitmap  as FM
 from dataio import *
 def testphase():
     #infile = 'data/HAT-365-0014102.tfalc' #pure outlier
-    infile = 'data/HAT-364-0000605.tfalc' #outlier + flatpart of curve
+    #infile = 'data/HAT-364-0000605.tfalc' #outlier + flatpart of curve
     #infile = 'data/HAT-317-0001481.ltf' #transit
+    infile = 'data/kplr011401755.rmtran'
     time = []; readcolumn(time,1,infile);time=np.array(time)
-    mag = []; readcolumn(mag,2,infile);mag=np.array(mag)
+    mag = []; readcolumn(mag,7,infile);mag=np.array(mag)
     #period = 16.3816800  
-    period = 6.1594446
+    #period = 6.1594446
     #period = 7.2491767
+    period = 13.8550595
     #epoch = 55513.5298468
-    epoch = 55508.9172669
+    #epoch = 55508.9172669
     #epoch = 55507.2281505
+    epoch = 2454974.3977524
     #q = 0.0109
-    q = 0.0127
+    #q = 0.0127
     #q = 0.0202
+    q = 0.03
     epoch += period*q/2.
     n0tran=round((time[0]-epoch-0.5*period)/period)
     if n0tran<0: 
         epoch+=n0tran*period
-    #n0tran=round((time[0]-epoch-0.5*period)/period)
-    #print n0tran,epoch
+    n0tran=round((time[0]-epoch-0.5*period)/period)
+   # print n0tran,epoch
     #print time.shape
-    phaselc = PF.PhaseLc(time,mag,period,epoch,10)
+    phaselc = PF.PhaseLc(time,mag,period,epoch)
     #phaselc.Foldts()
-    phaselc.TransitColor(0.04,400)
+    phaselc.TransitColor(2*q,400)
     return
 
 def testfitsigle():
-    data = np.loadtxt("data/color3.txt")
+    data = np.loadtxt("Kepler36b")
     #data = np.loadtxt("temp1")
     data = np.swapaxes(data,0,1).copy(order='C')
     fits = FM.Fitmap(data)
@@ -44,13 +48,14 @@ def testfitsigle():
     #fits.FitSingleFlat(10)
     #e2 = fits.Error()
     fits.FitTTV()
-    #e3 = fits.Error()
-    #fits.FitTTV(3)
+    e3 = fits.Error()
+    fits.FitTTV(3)
     #e4 = fits.Error()
+    print e3
     #print e1,e2
-    #print e3,e4
+    #print e2,e3,e4
     #print e1,e2,e3
-    fits.StdOutput()
+    #fits.StdOutput()    
     return
 
 if __name__ == '__main__':